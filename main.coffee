971c01005990dfc8b4bf4e625a86b340dadc7c2c
Andrew Courey <goldenmambasoft@gmail.com>
2018-10-13 11:08:56 -0400

    Everything but popup done

--- main.coffee
+++ main.coffee
@@ -3,8 +3,15 @@ String::capitalizeAllWords = ->
     a.toUpperCase()
 
 employeeHTML = ""
-displayRandomUser = ->
+employeeImage = null
 
+employeeName = null
+
+employeeEmail = null
+
+employeeLocation = null
+
+displayRandomUser = ->
     url = ' https://randomuser.me/api/ '
     handleErrors = (res) ->
       if !res.ok
@@ -15,26 +22,43 @@ displayRandomUser = ->
       res.json()
     updateProfile = (data) ->
         employeeImage = data.results[0].picture.large
-        console.log(data.results[0])
 
         employeeName = (data.results[0].name.first + " " + data.results[0].name.last).capitalizeAllWords()
 
         employeeEmail =  data.results[0].email
 
         employeeLocation = data.results[0].location.city.capitalizeAllWords()
-
+        console.log( data.results[0])
         employeeHTML += "<div class=\"employee\"><img class=\"employee-image\" src=\"#{employeeImage}\">
-            <h2 class=\"employee-name\">#{employeeName}</h2>
+            <div class=\"employee-info\"><h2 class=\"employee-name\">#{employeeName}</h2>
             <a class=\"employee-email\" href=\"mailto:#{employeeEmail}\">#{employeeEmail}</a>
-            <p class=\"employee-location\">#{employeeLocation}</p>
+            <p class=\"employee-location\">#{employeeLocation}</p>    </div>
         </div>"
         $("#employees").html(employeeHTML)
+
+        $('.employee').on 'click touchstart', ->
+          showPopUp = (picture, name, email, simpleLocation, phoneNumber,street, state, postCode)->
+            popUpHTML = "
+            <button id=\"exitPopup\">X</button>
+            <img src=\"#{picture}\" class=\"popup-Image\">
+            <h2 class=\"popup-name\">#{name}</h2>
+            <a href=\"mailto:#{email}\" class=\"popup-email\">#{email}</a>
+            <p class=\"popup-simpleLocation\">#{simpleLocation}</p>
+            <hr>
+            <a href=\"tel:#{phoneNumber}\">#{phoneNumber}</a>
+            <p class=\"popup-location\">#{street}, #{state}, #{postCode}</p>
+            <p class=\"popup-birthday\">Birthday: #{birthday}</p>
+            "
+            $("#popupContainer").html(popUpHTML)
+            showPopUp(employeeImage, employeeName, employeeEmail, employeeLocation,  data.results[0].cell,data.results[0].location.street, data.results[0].location,data.results[0].state, data.results[0].location.postcode)
+
+
+
+
         return
-
     fetch(url).then(handleErrors).then(parseJSON).then(updateProfile)
+
     undefined
-
-
 for i in [0..12]
     displayRandomUser()
     console.log("displayRandomUser")
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var displayRandomUser, employeeHTML, i, j;
+var displayRandomUser, employeeEmail, employeeHTML, employeeImage, employeeLocation, employeeName, i, j;
 
 String.prototype.capitalizeAllWords = function() {
   return this.replace(/(?:^|\s)\S/g, function(a) {
@@ -7,6 +7,14 @@ String.prototype.capitalizeAllWords = function() {
 };
 
 employeeHTML = "";
+
+employeeImage = null;
+
+employeeName = null;
+
+employeeEmail = null;
+
+employeeLocation = null;
 
 displayRandomUser = function() {
   var handleErrors, parseJSON, updateProfile, url;
@@ -22,14 +30,22 @@ displayRandomUser = function() {
     return res.json();
   };
   updateProfile = function(data) {
-    var employeeEmail, employeeImage, employeeLocation, employeeName;
     employeeImage = data.results[0].picture.large;
-    console.log(data.results[0]);
     employeeName = (data.results[0].name.first + " " + data.results[0].name.last).capitalizeAllWords();
     employeeEmail = data.results[0].email;
     employeeLocation = data.results[0].location.city.capitalizeAllWords();
-    employeeHTML += `<div class="employee"><img class="employee-image" src="${employeeImage}"> <h2 class="employee-name">${employeeName}</h2> <a class="employee-email" href="mailto:${employeeEmail}">${employeeEmail}</a> <p class="employee-location">${employeeLocation}</p> </div>`;
+    console.log(data.results[0]);
+    employeeHTML += `<div class="employee"><img class="employee-image" src="${employeeImage}"> <div class="employee-info"><h2 class="employee-name">${employeeName}</h2> <a class="employee-email" href="mailto:${employeeEmail}">${employeeEmail}</a> <p class="employee-location">${employeeLocation}</p>    </div> </div>`;
     $("#employees").html(employeeHTML);
+    $('.employee').on('click touchstart', function() {
+      var showPopUp;
+      return showPopUp = function(picture, name, email, simpleLocation, phoneNumber, street, state, postCode) {
+        var popUpHTML;
+        popUpHTML = `<button id="exitPopup">X</button> <img src="${picture}" class="popup-Image"> <h2 class="popup-name">${name}</h2> <a href="mailto:${email}" class="popup-email">${email}</a> <p class="popup-simpleLocation">${simpleLocation}</p> <hr> <a href="tel:${phoneNumber}">${phoneNumber}</a> <p class="popup-location">${street}, ${state}, ${postCode}</p> <p class="popup-birthday">Birthday: ${birthday}</p>`;
+        $("#popupContainer").html(popUpHTML);
+        return showPopUp(employeeImage, employeeName, employeeEmail, employeeLocation, data.results[0].cell, data.results[0].location.street, data.results[0].location, data.results[0].state, data.results[0].location.postcode);
+      };
+    });
   };
   fetch(url).then(handleErrors).then(parseJSON).then(updateProfile);
   return void 0;
